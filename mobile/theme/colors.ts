dc8487bf554ef66720651b1fd110c5a56a15bdc7
bugrayaktiyol <bugrayaktiyol@gmail.com>
2025-04-21 01:49:22 +0300

    feat: theme common colors

---
+++ mobile/theme/colors.ts
@@ -0,0 +1,32 @@
+const colors = {
+  
+  primary: '#4338ca',
+  primaryGradient: ['#943DFF', '#2938DC'] as readonly [string, string],
+  secondary: '#EF4444',
+  
+  background: {
+    dark: '#09090B',
+    card: '#27272A',
+    light: '#2a2a2a',
+    popup: '#18181B',
+  },
+  
+  text: {
+    primary: '#FAFAFA',
+    secondary: '#A1A1AA',
+    tertiary: '#71717A',
+    placeholder: '#666',
+  },
+  
+  ui: {
+    white: '#fff',
+    black: '#000',
+    success: '#4caf50',
+    error: '#EF4444',
+    errorLight: '#F37C7C',
+    gray: '#333',
+    darkGray: '#1a1a1a',
+  },
+};
+
+export default colors;
